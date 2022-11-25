85bab54f18cd5e839fb5156d828fcb7434603b57
Gianpaolo Papaiz <gianpaolopapaiz@hotmail.com>
2022-11-25 12:53:41 -0300

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: 'accounts#index'
+
+  resources :accounts do
+    resources :transactions, only: %i[index new create]
+    get '/ofx_import', to: 'accounts#ofx_import'
+    post '/ofx_import', to: 'accounts#ofx_import_to_account'
+  end
+
+  resources :categories do
+    resources :subcategories, only: %i[index new create]
+  end
+
+  resources :transactions, only: %i[show edit update destroy]
+  resources :subcategories, only: %i[edit update destroy]
 end
