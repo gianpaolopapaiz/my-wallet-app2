b1828d518611265a9e589be2a2fda166ca7dee5d
Gianpaolo Papaiz <gianpaolopapaiz@hotmail.com>
2023-01-08 13:04:13 -0300

    Add user and account validations associations and instance methods

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,3 +1,23 @@
 class Account < ApplicationRecord
+  # Associations
   belongs_to :user
+  has_many :transactions, dependent: :destroy
+  # Validations
+  validates :name, length: { maximum: 20 }, presence: true
+  validates :description, length: { maximum: 50 }
+  validates :initial_amount, numericality: true
+
+  def balance
+    initial_amount + transactions.sum(:value)
+  end
+
+  def balance_by_date(date)
+    initial_amount + transactions.where('date <= :date', date:).sum(:value)
+  end
+
+  def self.collection_balance
+    sum = 0.00
+    all.each { |account| sum += (account.balance || 0.00) }
+    sum
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ApplicationRecord
+  # Associations
+  has_many :categories
+  has_many :accounts
+  has_many :transactions, through: :accounts
+  # has_many :subcategories, through: :categories
+  # Validations
+  validates :first_name, :last_name, presence: true
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
