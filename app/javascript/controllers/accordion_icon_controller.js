50ce8a18b86226af86b999499a9e60a5a79842dd
Gianpaolo Papaiz <gianpaolopapaiz@hotmail.com>
2023-01-18 23:25:14 -0300

    Fix icons and buttons

--- app/javascript/controllers/accordion_icon_controller.js
+++ app/javascript/controllers/accordion_icon_controller.js
@@ -4,13 +4,13 @@ export default class extends Controller {
   connect() {
     let accordionButtonsSelector = document.querySelectorAll('.wallet-accordion-button');
     const switchAccordionButton = (e) => {
-      let accordionIcon = e.currentTarget.querySelector('i');
-      if (accordionIcon.className.includes('bi-caret-up-fill')) {
-        accordionIcon.classList.remove('bi-caret-up-fill');
-        accordionIcon.classList.add('bi-caret-down-fill');
+      let accordionIcon = e.currentTarget.querySelector('svg');
+      if (accordionIcon.className.baseVal.includes('down')) {
+        accordionIcon.classList.remove('fa-caret-down')
+        accordionIcon.classList.add('fa-caret-up')
       } else {
-        accordionIcon.classList.remove('bi-caret-down-fill');
-        accordionIcon.classList.add('bi-caret-up-fill');
+        accordionIcon.classList.remove('fa-caret-up')
+        accordionIcon.classList.add('fa-caret-down')
       }
     }
     accordionButtonsSelector.forEach((accordionButton) => {
