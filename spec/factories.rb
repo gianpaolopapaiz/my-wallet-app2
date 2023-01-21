61e4d57661972f2f9a6ffd56b8f865720f83848f
Gianpaolo Papaiz <gianpaolopapaiz@hotmail.com>
2023-01-21 11:07:24 -0300

    Fix factory bot config

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,20 +1,31 @@
 FactoryBot.define do
   factory :transaction do
-    title { "Transaction 1" }
+    name { "Transaction 1" }
     description { "Transaction 1 description" }
-    amount { 10.5 }
+    value { 10.5 }
     date { "2022-11-08" }
     category { nil }
+    subcategory { nil }
     account { nil }
   end
 
   factory :category do
     name { 'Category 1' }
     description { 'Category 1 description' }
+    user { nil }
+    subcategory { nil }
+  end
+
+  factory :subcategory do
+    name { 'Subcategory 1' }
+    description { 'Subcategory 1 description' }
+    user { nil }
+    subcategory { nil }
   end
 
   factory(:user) do
-    name { 'Sample User' }
+    first_name { 'Sample' }
+    last_name { 'User' }
     email { 'user@email.com' }
     password { 'password' }
   end
@@ -22,5 +33,6 @@ FactoryBot.define do
   factory(:account) do
     name { 'Account 1' }
     description { 'Account 1 description' }
+    initial_amount { 0 }
   end
 end
