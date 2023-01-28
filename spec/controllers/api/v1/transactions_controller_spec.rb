5ab1160b2fabe3a33e09e17ed3ca697d43545068
Gianpaolo Papaiz <gianpaolopapaiz@hotmail.com>
2023-01-28 00:07:47 -0300

    Add transaction controller specs

--- spec/controllers/api/v1/transactions_controller_spec.rb
+++ spec/controllers/api/v1/transactions_controller_spec.rb
@@ -23,6 +23,17 @@ RSpec.describe Api::V1::TransactionsController, type: :request do
     expect(parsed_body.last['id']).to eq(t2.id)
   end
 
+  it "SHOW - gets a category related to the user" do
+    @transaction = create(:transaction, account: @account)
+
+    get "/api/v1/accounts/#{@account.id}/transactions/#{@transaction.id}"
+    parsed_body = JSON.parse(response.body)
+
+    expect(response.status).to eq(200)
+    expect(parsed_body['id']).to eq(@transaction.id)
+    expect(parsed_body['name']).to eq('Transaction 1')
+  end
+
   it "CREATE - creates a transaction related to an account" do
     transaction_count = Transaction.all.size
     current_date = Date.current
@@ -47,5 +58,17 @@ RSpec.describe Api::V1::TransactionsController, type: :request do
     expect(parsed_body['value']).to eq(10.5)
     expect(parsed_body['date']).to eq(current_date.strftime('%Y-%m-%d'))
     expect(parsed_body['account_id']).to eq(@account.id)
+  end
+
+  it "DESTROY - destroys a transaction" do
+    @transaction = create(:transaction, account: @account)
+    transactions_count = Transaction.all.size
+
+    delete "/api/v1/accounts/#{@account.id}/transactions/#{@transaction.id}"
+    parsed_body = JSON.parse(response.body)
+
+    expect(response.status).to eq(200)
+    expect(Transaction.all.size).to eq(transactions_count - 1)
+    expect(parsed_body['message']).to eq('Transaction successfully destroyed')
   end
 end
